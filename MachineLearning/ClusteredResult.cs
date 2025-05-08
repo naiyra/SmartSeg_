16aa40f8f0ded859372270ec2c7e77ae4c76f2c6
naiyra <naiyrakad2@gmail.com>
2025-05-08 14:36:08 +0300

    files restructures

---
+++ MachineLearning/ClusteredResult.cs
@@ -0,0 +1,16 @@
+﻿using SmartSeg.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSeg.MachineLearning
+{
+    public class ClusteredResult
+    {
+        public IDataFrame OriginalWithLabels { get; set; }
+        public int OptimalK { get; set; }
+        public Dictionary<int, List<int>> ClusterAssignments { get; set; }
+    }
+}
--- Reporting/SegmentInterpreter.cs
+++ Reporting/SegmentInterpreter.cs
@@ -1,4 +1,4 @@
-﻿using SmartSeg.Core;
+﻿using SmartSeg.MachineLearning;
 using System;
 using System.Collections.Generic;
 using System.Linq;
